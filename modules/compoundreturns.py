08f1d198d21041622aa7eb1e06b4612ade1ee0dd
Scott Zastrow <scott@zastrow.com>
2023-04-12 16:46:17 -0500

    Added Input Fields

--- modules/compoundreturns.py
+++ modules/compoundreturns.py
@@ -1,6 +1,10 @@
 #Title: Compounding Returns function
 #Author: Scott Zastrow
+
 from __future__ import division
+import msvcrt
+
+count = 0
 
 def returns(rr, tp):
     '''
@@ -10,8 +14,40 @@ def returns(rr, tp):
         result (float) : The float result
         fs (string) : The formated string result
     '''
+
     result = (((1+rr)**tp)-1) *100
     
     fs =  "{0:.2f}%".format(result)
 
-    print(f'Return Rate: {"{0:.2f}%".format(rr*100)}\nTime Period: {tp}\nCompounded Return: {fs}')
+    print("\n---------------")
+    print("Output Results:")
+    print("---------------")
+    print(f'Return Rate: {"{0:.2f}%".format(rr*100)}\nTime Period: {tp}\nCompounded Return: {fs}\n')
+    global count
+    if count < 1:
+        count = 1
+        enterReturns()
+
+def enterReturns():
+    print("\n---------------")
+    print("Note: .01 is 1%")
+    print("---------------")
+
+    while True:
+        data = input("Enter Return rate: ")
+        try:
+            rate = float(data)
+            break
+        except:
+            continue
+            
+    while True:
+        data = input("Enter Time period: ")
+        try:
+            time = float(data)
+            break
+        except:
+            continue       
+    returns(rate,time)
+        
+
